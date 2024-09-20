1ede7a6b4105c002916334e5d70a56b3eb43cba6
noritsune <bill.gatess4037@gmail.com>
2024-09-21 00:22:56 +0900

    ダブルクリックで実行するためのrun.batを作成

---
+++ run.bat
@@ -0,0 +1,4 @@
+call "D:\Users\owner\anaconda3\Scripts\activate.bat"
+call activate deep-live-cam
+
+"D:\Users\owner\anaconda3\envs\deep-live-cam\python.exe" run.py --execution-provider cuda --execution-threads 60 --max-memory 60
